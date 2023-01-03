4e65c9f6f6842289d7e20b8f0cffafeb7c5b23a0
Harsh6575 <harsh.190410107146@gmail.com>
2023-01-03 12:39:13 +0530

    working on createContainer 4:16

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { MdFastfood } from 'react-icons/md';
+import { MdAttachMoney, MdCloudUpload, MdDelete, MdFastfood, MdFoodBank } from 'react-icons/md';
 import { catagories } from '../utils/data';
 import Loader from './Loader';
+import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../firebase.config';
 
 const CreateContainer = () => {
   const [title, setTitle] = useState('');
@@ -15,6 +18,94 @@ const CreateContainer = () => {
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const uploadImage = (e) => {
+    setIsLoading(true);
+    const imageFile = e.target.files[0];
+    const storageRef = ref(storage, `Images/${Date.now()}- ${imageFile.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    uploadTask.on('state_changed', (snapshot) => {
+      const uploadProgress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    }, (error) => {
+      console.log(error);
+      setFields(true);
+      setAlertStatus('danger');
+      setMsg('Error uploading image. Please try again later.');
+      setTimeout(() => {
+        setFields(false);
+        setIsLoading(false);
+      }, 4000);
+    }, () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setImageAsset(downloadURL);
+        setIsLoading(false);
+        setFields(true);
+        setAlertStatus('success');
+        setMsg('Image uploaded successfully.');
+        setTimeout(() => {
+          setFields(false);
+        }, 4000);
+      });
+    });
+  };
+  const deleteImage = () => {
+    setIsLoading(true);
+    const deleteRef = ref(storage, imageAsset);
+    deleteObject(deleteRef).then(() => {
+      setImageAsset(null);
+      setIsLoading(false);
+      setFields(true);
+      setAlertStatus('success');
+      setMsg('Image deleted successfully.');
+      setTimeout(() => {
+        setFields(false);
+      }, 4000);
+    }).catch((error) => {
+      console.log(error);
+      setFields(true);
+      setAlertStatus('danger');
+      setMsg('Error deleting image. Please try again later.');
+      setTimeout(() => {
+        setFields(false);
+        setIsLoading(false);
+      }, 4000);
+    });
+  };
+  const saveDetails = () => {
+    setIsLoading(true);
+    try {
+      if ((!title || !calories || !price || !category || !imageAsset)) {
+        setFields(true);
+        setAlertStatus('danger');
+        setMsg('Please fill all the fields.');
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+        return;
+      } else {
+        const data = {
+          id: Date.now(),
+          title: title,
+          imageURL: imageAsset,
+          calories: calories,
+          category:category,
+          price: price,
+          qty: 1
+        }
+      }
+
+    } catch (error) {
+      console.log(error);
+      setFields(true);
+      setAlertStatus('danger');
+      setMsg('Error while saving details. Please try again later.');
+      setTimeout(() => {
+        setFields(false);
+        setIsLoading(false);
+      }, 4000);
+    }
+  };
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center'>
@@ -44,7 +135,41 @@ const CreateContainer = () => {
           </select>
         </div>
         <div className='group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg'>
-          {isLoading && <Loader />}
+          {isLoading ? <Loader /> : <>
+            {!imageAsset ? <>
+              <label className='w-full h-full flex flex-col items-center justify-center cursor-pointer'>
+                <div className='w-full h-full flex flex-col items-center justify-center'>
+                  <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700' />
+                  <p className='text-gray-500 hover:text-gray-700'>
+                    Click here to upload image
+                  </p>
+                </div>
+                <input type='file' name='uploadimage' accept='image/*' onChange={uploadImage} className='w-0 h-0' />
+              </label>
+            </> : <>
+              <div className='relative h-full'>
+                <img src={imageAsset} alt='upload image' className="w-full h-full object-cover" />
+                <button type='button' className='absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out' onClick={deleteImage}>
+                  <MdDelete className='text-white' />
+                </button>
+              </div>
+            </>
+            }
+          </>
+          }
+        </div>
+        <div className='w-full flex flex-col md:flex-row items-center gap-3'>
+          <div className='w-full p-2 border-b border-gray-200 flex items-center gap-2'>
+            <MdFoodBank className='text-2xl text-gray-700' />
+            <input type='text' required placeholder='calories' value={calories} onChange={(e) => setCalories(e.target.value)} className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-500' />
+          </div>
+          <div className='w-full p-2 border-b border-gray-200 flex items-center gap-2'>
+            <MdAttachMoney className='text-2xl text-gray-700' />
+            <input type='text' required placeholder='price' value={price} onChange={(e) => setPrice(e.target.value)} className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-500' />
+          </div>
+        </div>
+        <div className='flex items-center w-full'>
+          <button type='button' className='ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-3 rounded-lg text-lg text-white tracking-[5px]' onClick={saveDetails}>Save</button>
         </div>
       </div>
     </div>
